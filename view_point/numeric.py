3b43b480ef4932efe9caa305081e57054f5a0159
Tran duy Phu <phusdt@Trans-MacBook-Air.local>
2020-07-22 15:07:57 +0700

    update check_numeric

--- view_point/numeric.py
+++ view_point/numeric.py
@@ -12,8 +12,38 @@ def check_numeric(data_col, threshold):
     :return: result (OK, NG, NA), k (numbers of numeric rows), N (total values),
     abnormal_data_details (DataFrame contains values and indexes of invalid numeric).
      """
+    #create dictionnary to constains values and indexes of invalid numeric
+    abnormal_data_details = {'Indexes': [], 'Values': []}
 
-    #TODO: Write code to check numerical view point
+    indexes = []
+    values = []
+    #for loop to fill data to 2 list (indexes and values)
+    for index, value in data_col.iteritems():
+        row_isnumeric = value.isnumeric()
+        if not row_isnumeric:
+            indexes.append(index)
+            values.append(value)
+
+    #update value in dict and change into data frame
+    abnormal_data_details['Indexes'] = indexes
+    abnormal_data_details['Values'] = values
+    abnormal_data_details = pd.DataFrame(abnormal_data_details)
+
+    result = 'NA'
+    k = len(values)  # number of numeric rows
+    N = len(data_col)  # total values
+    #Condition to get result by the ratio of k/N and theshold
+    if k/N >= threshold and k < N:
+        result = 'NG'
+    elif k == N:
+        result = 'OK'
+
+    return result, k, N, abnormal_data_details
+
+
+
+
+
 
 
 
